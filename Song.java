5fa71d02eb44d50a80e868c02d60581343e852ca
Jujube Hutt <jhutt27@students.polytechnic.org>
2025-01-24 10:09:45 -0800

    Brought ind files from Playlist.zip

---
+++ Song.java
@@ -0,0 +1,30 @@
+/**
+ * Created a Song object with attributes for the Playlist Project
+ * @author Jujube Hutt 
+ * @version 2025-01-24
+ */
+public class Song {
+    //Fields-- what information do we want each Song to store?
+
+
+
+
+    /**
+     * Constructor-- what information needs to be given to make a Song?
+     * How will you handle 'liked' songs? It makes sense for a Song not be 'liked' by default
+     */
+
+
+
+
+
+     /**
+      * Methods-- what will you want each Song to do?
+      * Consider all the getter methods (getName, getArtist, etc.)
+      * You should probably have a toString method to be able to get the information for the full
+      * song easily as well!
+      * What kind of mutator (setter) methods will you need?
+      */
+
+
+}
